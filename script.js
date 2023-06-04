efa72a9b64b0b6c0fe2477b378b9ec594e27e878
GilazovDEV <azalialebedeva6@gmail.com>
2023-06-04 22:28:46 +0500

    add save log

--- script.js
+++ script.js
@@ -1,72 +1,113 @@
-const express = require("express");
-const unirest = require("unirest");
+// const express = require("express");
+// const unirest = require("unirest");
+// const session = require("express-session");
 
-const app = express();
+// const app = express();
 
-app.use("/assets", express.static("assets"));
-app.set("view engine", "ejs");
+// app.use("/assets", express.static("assets"));
+// app.set("view engine", "ejs");
 
-app.get("/", (request, response) => {
-  if (request.query.code) {
-    let clientID = "1095721219275358330";
-    let redirect_uri = "http://127.0.0.1:5000/";
-    let clientSecret = "BoneeleOyQAMYNKxB8QOc0RPPGVP0qIw";
-    let requestPayload = {
-      redirect_uri,
-      client_id: clientID,
-      grant_type: "authorization_code",
-      client_secret: clientSecret,
-      code: request.query.code,
-    };
-    unirest
-      .post("https://discordapp.com/api/oauth2/token")
-      .send(requestPayload)
-      .headers({
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "DiscordBot",
-      })
-      .then((data) => {
-        unirest
-          .get("https://discordapp.com/api/users/@me")
-          .headers({
-            Authorization: `${data.body.token_type} ${data.body.access_token}`,
-          })
-          .then((userData) => {
-            console.log(userData.body);
-            const avatarUrl = `https://cdn.discordapp.com/avatars/${userData.body.id}/${userData.body.avatar}.png`;
-            const bannerUrl = userData.body.banner ? `https://cdn.discordapp.com/banners/${userData.body.id}/${userData.body.banner}.png` : null;
-            const username = userData.body.username;
-            const discriminator = userData.body.discriminator;
-            response.render("account", { avatarUrl, bannerUrl, username, discriminator }); // передаем URL аватара, баннера, ника и дискриминатора в шаблон
-          })
-          .catch((err) => {
-            console.log(err);
-            response.redirect("/index");
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-        response.redirect("/index");
-      });
-  } else {
-    response.render("index");
-  }
-});
+// // Добавляем настройки сессии
+// app.use(
+//   session({
+//     secret: "secret-key",
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
 
-app.get("/account", (request, response) => {
-  response.render("account");
-});
+// app.get("/", (request, response) => {
+//   if (request.query.code) {
+//     let clientID = "1095721219275358330";
+//     let redirect_uri = "http://127.0.0.1:5000/";
+//     let clientSecret = "BoneeleOyQAMYNKxB8QOc0RPPGVP0qIw";
+//     let requestPayload = {
+//       redirect_uri,
+//       client_id: clientID,
+//       grant_type: "authorization_code",
+//       client_secret: clientSecret,
+//       code: request.query.code,
+//     };
+//     unirest
+//       .post("https://discordapp.com/api/oauth2/token")
+//       .send(requestPayload)
+//       .headers({
+//         "Content-Type": "application/x-www-form-urlencoded",
+//         "User-Agent": "DiscordBot",
+//       })
+//       .then((data) => {
+//         // Сохраняем информацию о пользователе в сессии
+//         request.session.tokenType = data.body.token_type;
+//         request.session.accessToken = data.body.access_token;
+//         request.session.save();
 
-app.get("/notifications", (request, response) => {
-  response.render("notifications");
-});
+//         // Редирект на страницу аккаунта
+//         response.redirect("/account");
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         response.redirect("/index");
+//       });
+//   } else {
+//     response.render("index");
+//   }
+// });
 
-app.get("/transation", (request, response) => {
-  response.render("transation");
-});
+// // Добавляем проверку авторизации перед отображением защищенных страниц
+// app.get("/account", isAuthenticated, (request, response) => {
+//   // Достаем информацию о пользователе из сессии
+//   const tokenType = request.session.tokenType;
+//   const accessToken = request.session.accessToken;
 
-app.listen(5000, () => {
-  console.log("server load in 5000 port");
-});
+//   unirest
+//     .get("https://discordapp.com/api/users/@me")
+//     .headers({
+//       Authorization: `${tokenType} ${accessToken}`,
+//     })
+//     .then((userData) => {
+//       console.log(userData.body);
+//       const avatarUrl = `https://cdn.discordapp.com/avatars/${userData.body.id}/${userData.body.avatar}.png`;
+//       const bannerUrl = userData.body.banner
+//         ? `https://cdn.discordapp.com/banners/${userData.body.id}/${userData.body.banner}.png`
+//         : null;
+//       const username = userData.body.username;
+//       const discriminator = userData.body.discriminator;
+//       response.render("account", {
+//         avatarUrl,
+//         bannerUrl,
+//         username,
+//         discriminator,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       response.redirect("/index");
+//     });
+// });
 
-// сделать проверку на айди в жсон
+// // Middleware для проверки авторизации
+// function isAuthenticated(request, response, next) {
+//   if (
+//     request.session &&
+//     request.session.tokenType &&
+//     request.session.accessToken
+//   ) {
+//     // Пользователь авторизован, продолжаем выполнение следующего обработчика
+//     return next();
+//   } else {
+//     // Пользователь не авторизован, перенаправляем на страницу авторизации
+//     response.redirect("/index");
+//   }
+// }
+
+// app.get("/notifications", (request, response) => {
+//   response.render("notifications");
+// });
+
+// app.get("/transation", (request, response) => {
+//   response.render("transation");
+// });
+
+// app.listen(5000, () => {
+//   console.log("Server is running on port 5000");
+// });
